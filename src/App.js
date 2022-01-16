89e7ab90e6ecdfd26a6ddac87c6f217b20478dc2
husaininajib <husaininajib98@gmail.com>
2022-01-16 17:20:05 +0800

    added reset function

--- src/App.js
+++ src/App.js
@@ -1,71 +1,106 @@
 import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+// import Confetti from './components/Confetti';
+// import Confetti from "react-confetti"
 import { nanoid } from 'nanoid'
 import './App.css';
 
 
 function App() {
-  const [numbers, setNumbers] = useState(generateNumbers())
+  const [dice, setDice] = useState(generateRandomDice())
+  const [tenzies, setTenzies] = useState(false)
 
-  function generateNumbers() {
-    const numbersArr = []
-    for (let i = 0; i < 10; i++) {
-      numbersArr.push({
-        id: nanoid(),
-        randomValue: Math.ceil(Math.random() * 6),
-        isHeld: false
-      })
+  // FUNCTION FOR COMPONENTS
+  
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
     }
-    return numbersArr
+  }
+
+  function generateRandomDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie())
+    }
+    return newDice
   }  
 
-  const allNumbers = numbers.map(number => {
-    return (
-      <Main
-        key={number.id}
-        id={number.id}
-        value={number.randomValue}
-        toggle={handleClick}
-        isHeld={number.isHeld}
-        holdDice={holdDice}
-      />
-    )
-  })
-
   function rollDice() {
-    setNumbers(generateNumbers())
+    setDice(prev => {
+      const newDice = []
+      prev.forEach(item => {
+        item.isHeld? newDice.push(item) : newDice.push(generateNewDie())
+      })
+      return newDice
+    })
+    //  setDice(item => item.map(die => {
+    //         return die.isHeld ? 
+    //             die :
+    //             generateNewDie()
+    //     })) // other method
+  }
+
+  function reset() {
+    setDice(generateRandomDice())
+    setTenzies(false)
   }
 
   function holdDice(id) {
-    setNumbers(prevNumbers => {
-      const newNumbers = []
+    setDice(prevNumbers => {
+      const newDice = []
       prevNumbers.forEach(object => {
         if (object.id === id) {
           const copiedNumbers = {...object, isHeld: ! object.isHeld}
-          newNumbers.push(copiedNumbers) 
+          newDice.push(copiedNumbers) 
         } else {
-          newNumbers.push({...object})
+          newDice.push({...object})
         } 
       }) 
-      return newNumbers
+      return newDice
     })
   }
 
-  function handleClick(id) {
-      console.log("clicked")
-  }
+  useEffect(() => {
+      const allHeld = dice.every(item => item.isHeld === dice[0].isHeld)
+      const allSameValue = dice.every(item => item.value === dice[0].value)
+
+      if (allHeld && allSameValue) {
+        setTenzies(true)
+        console.log("you won")
+      }
+  }, [dice])
+  
+  const diceElements = dice.map(die => {
+  return (
+    <Main
+      key={die.id}
+      id={die.id}
+      dieValue={die.value}
+      isHeld={die.isHeld}
+      holdDice={holdDice}
+    />
+    )
+  })
+  
+
+  //COMPONENTS
+
   return (
     <div className="container border-8 border-black p-8 m-4 grid place-items-center">
+      {/* {tenzies && <Confetti />} */}
       <Header />
       <div className="card-container mt-8 grid grid-cols-5 gap-4">
-        {allNumbers}
+        {diceElements}
       </div>
       <button 
         className="mt-12 text-white bg-blue-600 te font-semibold py-1 px-4 rounded-md"
-        onClick={rollDice}
+        onClick= {tenzies? reset : rollDice} 
       >
-        Roll
+        {tenzies? "New Game" : "Roll"}  
       </button>
     </div>
   )
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,9 @@ export default function Main(props) {
     return (
             <div 
                 className={`${props.isHeld? "bg-green-400" : "bg-white"} number-card shadow-md w-12 h-12 grid place-items-center cursor-pointer`}
-                onClick={() => props.toggle(props.id)}
                 onClick={() => props.holdDice(props.id)}
             >
-                <span className="font-bold">{props.value}</span>
+                <span className="font-bold">{props.dieValue}</span>
             </div>
     )
 }
